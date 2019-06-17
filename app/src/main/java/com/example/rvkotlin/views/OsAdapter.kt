fa4c3d38b7ae11ab7efac0fc0f2e30a8d980fd69
mochammadfariz <mochammadfariz11@gmail.com>
2019-06-17 23:12:30 +0700

    adding adapter

--- app/src/main/java/com/example/rvkotlin/views/OsAdapter.kt
+++ app/src/main/java/com/example/rvkotlin/views/OsAdapter.kt
@@ -1,4 +1,29 @@
 package com.example.rvkotlin.views
 
-class OsAdapter {
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.rvkotlin.R
+import com.example.rvkotlin.model.AndroidOS
+import kotlinx.android.synthetic.main.adapter_android_os.view.*
+
+class OsAdapter(private val context: Context,private val listAndroidOS: List<AndroidOS>):RecyclerView.Adapter<OsAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OsAdapter.ViewHolder =
+        ViewHolder(LayoutInflater.from(context).inflate(R.layout.adapter_android_os,parent,false))
+
+
+    override fun getItemCount(): Int =
+        listAndroidOS.size
+
+
+    override fun onBindViewHolder(holder: OsAdapter.ViewHolder, position: Int) {
+            holder.bindAndroidOS(listAndroidOS[position])
+    }
+    inner class ViewHolder(itemView:View): RecyclerView.ViewHolder(itemView){
+        fun bindAndroidOS(androidOS: AndroidOS){
+            itemView.androidOSText.text = androidOS.android
+        }
+    }
 }
